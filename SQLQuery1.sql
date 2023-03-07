fe55c871d0215e0591974f23475da38d1a04d98e
mikeiavellii <mike.blackburn.541@gmail.com>
2023-03-07 09:48:58 -0500

    dropped tables, added prioirty variable, created several dummy fields to sort by

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -21,6 +21,8 @@
 --	);
 
 --INSERT INTO Ticket(UserId, Email, DateSubmitted, DateCompleted, SubjectBrief, FullIssue, [Open])
---VALUES ('JohnJohnson', 'jj@gmail.com', '03/03/2023', Null, 'Can''t access outlook', 'I have been trying to login into outlook but my password is incorrect.', 1);
-
-Select * from Ticket;
+--VALUES ('JohnJohnson', 'jj@gmail.com', 'Moderate Priority' '03/03/2023', Null, 'Can''t access outlook', 'I have been trying to login into outlook but my password is incorrect.', 1),
+--('StanSmith', 'ss@gmail.com', 'Low Priority','03/07/2023', Null, 'The CIA website is down', 'I have been trying to go to the CIA website to update the threat level to terror alert orange for an hour and it''s not working. How will American''s know to be moderately on edge about vauge looming threats without it??.', 1),
+--('DaleDenton', 'dd@gmail.com', 'High Priority', '03/06/2023', Null, 'My computer can''t connect to the internet', 'I have been trying to login into the internet for an hour, but it''s not working.', 1),
+--('ChrisChristie', 'cc@gmail.com', 'Critical Priority', '03/07/2023', Null, 'My computer won''t turn on', 'The screen hasn''t turned on since the power outage', 1);
+--Select * from Ticket;
